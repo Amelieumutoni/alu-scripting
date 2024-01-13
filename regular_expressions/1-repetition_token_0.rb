cea8fa0643a1b7eb98910d9b0b34ffcab401fa3c
Amelieumutoni <a.umutoni@alustudent.com>
2024-01-13 03:02:15 -0800

    1-repetition_token_0.rb

--- regular_expressions/1-repetition_token_0.rb
+++ regular_expressions/1-repetition_token_0.rb
@@ -1,13 +1,5 @@
 #!/usr/bin/env ruby
-
-# Define the regular expression to match the cases
 regex = /hbtt{1,5}n/
-
-# Get the argument from the command line
 arg = ARGV[0]
-
-# Find all matches of the regular expression in the argument
 matches = arg.scan(regex)
-
-# Print all matches, or an empty string if no matches
-puts matches.join
+puts matches.join  
